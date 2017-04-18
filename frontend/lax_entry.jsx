ff08f4092bb3f8dd12c1ff4c9073f3ac9accf68f
Szymon Wojnar <wojnar48@gmail.com>
2017-04-18 12:23:41 -0400

    Add front-end config files and /frontend directory

---
+++ frontend/lax_entry.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<div>hello</div>, document.getElementById('root'));
+});
