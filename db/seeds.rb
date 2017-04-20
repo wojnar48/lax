17355059cb81470f46b41986721d4e02741796ee
Szymon Wojnar <wojnar48@gmail.com>
2017-04-20 09:39:38 -0400

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
 ActiveRecord::Base.transaction do
+  User.destroy_all
+  User.create!(username: 'guest', password: 'password')
 
+  Chatroom.destroy_all
+  Chatroom.create!(name: 'general')
+  Chatroom.create!(name: 'random')
+  Chatroom.create!(name: 'january-2017')
 end
