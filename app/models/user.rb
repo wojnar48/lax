3664135776faed8ae354239f5b4944094a30b47d
Szymon Wojnar <wojnar48@gmail.com>
2017-05-01 09:16:12 -0400

    Remove paperclip avatar attachment

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,6 @@ class User < ApplicationRecord
   has_many :chatrooms, through: :chatroom_users
   has_many :messages
 
-  has_attached_file :avatar, default_url: "philoraptor.jpg"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-
   def self.generate_session_token
     SecureRandom.urlsafe_base64(16)
   end
