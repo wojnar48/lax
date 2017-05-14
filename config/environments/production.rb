cb32d2242fa438d9a18a406c19c8125e928f2ef9
Szymon Wojnar <wojnar48@gmail.com>
2017-05-14 16:00:38 -0400

    Prepare to push Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,8 +10,8 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.web_socket_server_url = "wss://lax-app.herokuapp.com/cable"
-  # config.web_socket_server_url = "wss://laxapp.online/cable"
+  # config.web_socket_server_url = "wss://lax-app.herokuapp.com/cable"
+  config.web_socket_server_url = "wss://laxapp.online/cable"
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
