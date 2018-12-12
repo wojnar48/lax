1e029fed74fea78e3de78a8d9f9d1df97c4017e8
Szymon Wojnar <wojnar48@gmail.com>
2018-12-12 18:30:43 -0500

    Update db and pusher config

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,7 +1,9 @@
 require 'pusher'
 
-Pusher.app_id = ENV["pusher_app_id"]
-Pusher.key = ENV["pusher_key"]
-Pusher.secret = ENV["pusher_secret"]
+# TODO(SW): Revert back to encrypting once TLS is configured
+Pusher.app_id = ENV['PUSHER_APP_ID']
+Pusher.key = ENV['PUSHER_KEY']
+Pusher.secret = ENV['PUSHER_SECRET']
+Pusher.cluster = ENV['PUSHER_CLUSTER']
 Pusher.logger = Rails.logger
-Pusher.encrypted = true
+# Pusher.encrypted = true
