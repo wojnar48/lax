a8eb1ca9d8deef98f6b5016e22cda25998114744
Szymon Wojnar <wojnar48@gmail.com>
2018-12-17 17:19:56 -0500

    Run pusher in encrypted mode

--- frontend/components/chat/chat.jsx
+++ frontend/components/chat/chat.jsx
@@ -15,10 +15,8 @@ import { fetchSubscriptions, createSubscription } from '../../actions/subscripti
 class Chat extends Component {
   constructor() {
     super();
-    // TODO(SW): Enable encryption once TLS is configured
     // TODO(SW): Move pusher client setup into lib folder
-    // this.pusher = new Pusher('a514cb9081b7cf5aace9', { encrypted: true });
-    this.pusher = new Pusher('a514cb9081b7cf5aace9');
+    this.pusher = new Pusher('a514cb9081b7cf5aace9', { encrypted: true });
   }
 
   componentDidMount() {
