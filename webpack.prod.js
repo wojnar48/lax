bcd2d3b5b56d3b459939602d59dbe945f78e6d59
Szymon Wojnar <wojnar48@gmail.com>
2018-12-17 16:29:28 -0500

    Split webpack config into dev and prod

---
+++ webpack.prod.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: './frontend/lax_entry.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app/assets/javascripts'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              'react',
+              [
+                "env",
+                {
+                  "targets": {
+                    "browsers": ["last 2 versions"]
+                  }
+                }
+              ]
+            ],
+            plugins: [
+              'transform-object-rest-spread',
+              'transform-class-properties'
+            ]
+          }
+        },
+      },
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx', '*']
+  },
+};
+  
